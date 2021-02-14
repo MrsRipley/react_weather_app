1886a15365387c71f1a37da5db1f4e3c68efc246
Mckenna Ripley <mckennaripley@Mckennas-Mac-mini.local>
2021-02-13 22:52:15 -0600

    set up HTML and added CSS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by McKenna Ripley and is{" "}
-        <a href="https://heuristic-cori-0dbca2.netlify.app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by McKenna Ripley and is{" "}
+          <a href="https://heuristic-cori-0dbca2.netlify.app" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
