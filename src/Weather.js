79ed6093b2384810495e82060a9c6cc2251a7517
Mckenna Ripley <mckennaripley@Mckennas-Mac-mini.local>
2021-02-14 00:09:19 -0600

    adjusted styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
@@ -32,16 +33,19 @@ export default function Weather() {
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
+            className="float-left"
           />
-          6°C
+          <div className="float-left"></div>
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 15 km/hr</li>
-          </ul>
-        </div>
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Precipitation: 15%</li>
+          <li>Humidity: 72%</li>
+          <li>Wind: 15 km/hr</li>
+        </ul>
       </div>
     </div>
   );
