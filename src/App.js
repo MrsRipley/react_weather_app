9b4fa32402f0f485aeecc766420f081a711b96b9
Mckenna Ripley <mckennaripley@Mckennas-Mac-mini.local>
2021-02-13 20:32:59 -0600

    Added Footer.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import react from "react";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by McKenna Ripley and is{" "}
+        <a href="https://heuristic-cori-0dbca2.netlify.app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
