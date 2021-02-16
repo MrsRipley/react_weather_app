124aa5327dd279302d38a297a1ae3ba8f3ed40ae
Mckenna Ripley <mckennaripley@Mckennas-Mac-mini.local>
2021-02-15 23:58:47 -0600

    Added in Forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForecastPreview from "./WeatherForecastPreview";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+  if (loaded) {
+    return (
+      <div className="WeatherForecast row">
+        <WeatherForecastPreview data={forecast.list[0]} />
+      </div>
+    );
+  } else {
+    let apiKey = "82b5e006082675254d9d4e16c9c06f3e";
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleForecastResponse);
+
+    return null;
+  }
+}
